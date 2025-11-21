56db59209f7a4ad8033873ab50de8ad2e9a82eae
Justin Duong <jduong@umich.edu>
2025-11-20 19:46:18 -0500

    removed drawer work.

--- app/drawer.jsx
+++
@@ -1,8 +0,0 @@
-const Drawer = ({isOpen}) => {
-  console.log(isOpen);
-  return (
-    <div id="cart-drawer" className={`w-1/3 h-full bg-green-500 fixed transform transition-transform duration-300 ease-in-out right-0 ${isOpen ? "translate-x-0" : "translate-x-full"}`}></div>
-  )
-}
-
-export default Drawer;
