149e68b33b6e40b5662b6c94c5ef08ea301d967b
Justin Duong <jduong@umich.edu>
2025-11-20 19:45:18 -0500

    Adding Drawer Component.

--- app/drawer.jsx
+++ app/drawer.jsx
@@ -1,5 +1,4 @@
 const Drawer = ({isOpen}) => {
-  console.log(isOpen);
   return (
     <div id="cart-drawer" className={`w-1/3 h-full bg-green-500 fixed transform transition-transform duration-300 ease-in-out right-0 ${isOpen ? "translate-x-0" : "translate-x-full"}`}></div>
   )
